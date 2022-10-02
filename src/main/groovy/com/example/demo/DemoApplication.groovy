4aba427c554a2512888771644e5265e0f6ccdd59
josephedward <josephedwardwork@gmail.com>
2022-10-01 23:04:10 -0400

    controller/route implementation

--- src/main/groovy/com/example/demo/DemoApplication.groovy
+++ src/main/groovy/com/example/demo/DemoApplication.groovy
@@ -9,46 +9,13 @@
 
 package com.example.demo
 
-// import org.springframework.boot.SpringApplication
-// import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
 
-// @SpringBootApplication
+@SpringBootApplication
 class DemoApplication {
 
     static void main(String[] args) {        
-
-        def file = new File('transactions.csv')
-        def rows = file.readLines().tail()*.split(',')
-        int total = rows.size()
-        println "Total number of transactions: $total"
-
-        def customerScores = [:]
-
-        for (x in rows) {
-            def customer = x[0]
-            def month = x[1]
-            def amount = x[2].toInteger()
-            def points = 0
-
-            if (amount > 100) {
-                points += (amount - 100) * 2
-            }
-            if (amount > 50) {
-                points += (amount - 50)
-            }
-
-
-            if (customerScores[customer] == null) {
-                customerScores[customer] = 0
-            }
-            customerScores[customer] = customerScores[customer]+points
-
-        }
-
-        println "Customer Scores: $customerScores"
-
-
+        SpringApplication.run(DemoApplication, args)
     }
-
-
 }
---
+++ src/main/groovy/com/example/demo/TransactionController.groovy
@@ -0,0 +1,41 @@
+package com.example.demo
+
+//import requestMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class transactionController {
+
+    @RequestMapping("/transactions")
+    String transactions() {
+                def file = new File('transactions.csv')
+        def rows = file.readLines().tail()*.split(',')
+        int total = rows.size()
+        println "Total number of transactions: $total"
+
+        def customerScores = [:]
+
+        for (x in rows) {
+            def customer = x[0]
+            def month = x[1]
+            def amount = x[2].toInteger()
+            def points = 0
+
+            if (amount > 100) {
+                points += (amount - 100) * 2
+            }
+            if (amount > 50) {
+                points += (amount - 50)
+            }
+
+            if (customerScores[customer] == null) {
+                customerScores[customer] = 0
+            }
+            customerScores[customer] = customerScores[customer]+points
+        }
+        println "Customer Scores: $customerScores"
+
+        return "Customer Scores: $customerScores"
+    }
+}
